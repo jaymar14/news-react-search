a2b2b88fcbfed5bcfb4209417638d337f1242705
Jay Martinez <jay.martinez7@yahoo.com>
2017-12-20 00:59:07 -0600

    files added

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+	return(
+		<div className='jumbotron container'>
+			<h1>NY Times Articles</h1>
+			<hr/>
+			<h4>Search Interesting Articles!</h4>
+			</div>
+			)
+};
+
+export default Header;
---
+++ client/src/components/Search/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+ const Button = (props) => {
+ 	return(
+ 		<button onClick={props.handleButtonClick} className='btn btn-danger btn-lg'>Search</button>
+ 		)	
+ };
+
+ export default Button;
